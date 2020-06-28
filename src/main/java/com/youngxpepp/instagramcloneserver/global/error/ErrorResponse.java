da5e407a3b960d62d4e064994296856b71f3d17a
youngxpepp <youngxpepp@gmail.com>
2020-06-28 16:36:30 +0900

    에러 응답의 구조 변경 (resolve #9)

--- src/main/java/com/youngxpepp/instagramcloneserver/global/error/ErrorResponse.java
+++ src/main/java/com/youngxpepp/instagramcloneserver/global/error/ErrorResponse.java
@@ -1,85 +1,21 @@
 package com.youngxpepp.instagramcloneserver.global.error;
 
+import com.youngxpepp.instagramcloneserver.global.error.type.DefaultErrorType;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class ErrorResponse {
 
-    private LocalDateTime timestamp;
-    private int status;
-    private String error;
-    private String message;
-    private List<FieldError> fieldErrors;
-    private int code;
+    private DefaultErrorType error;
 
-    public ErrorResponse(ErrorCode errorCode) {
-        this.timestamp = LocalDateTime.now();
-        this.status = errorCode.getHttpStatus().value();
-        this.error = errorCode.getHttpStatus().getReasonPhrase();
-        this.message = errorCode.getMessage();
-        this.fieldErrors = new ArrayList<>();
-        this.code = errorCode.getCode();
-    }
-
-    public ErrorResponse(ErrorCode errorCode, BindingResult bindingResult) {
-        this(errorCode);
-        this.fieldErrors = FieldError.of(bindingResult);
-    }
-
-    public ErrorResponse(ErrorCode errorCode, List<FieldError> fieldErrors) {
-        this(errorCode);
-        this.fieldErrors = fieldErrors;
-    }
-
-    public ErrorResponse(MethodArgumentTypeMismatchException e) {
-        this(ErrorCode.INVALID_TYPE_VALUE);
-        String value = e.getValue() == null ? "" : e.getValue().toString();
-        this.fieldErrors.addAll(FieldError.of(e.getName(), value, e.getErrorCode()));
+    public ErrorResponse(DefaultErrorType error) {
+        this.error = error;
     }
 
     public ResponseEntity<ErrorResponse> responseEntity() {
-        return new ResponseEntity<>(this, HttpStatus.resolve(this.status));
-    }
-
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @Getter
-    public static class FieldError {
-
-        private String field;
-        private String value;
-        private String reason;
-
-        private FieldError(String field, String value, String reason) {
-            this.field = field;
-            this.value = value;
-            this.reason = reason;
-        }
-
-        public static List<FieldError> of(String field, String value, String reason) {
-            List<FieldError> fieldErrors = new ArrayList<>();
-            fieldErrors.add(new FieldError(field, value, reason));
-            return fieldErrors;
-        }
-
-        public static List<FieldError> of(BindingResult bindingResult) {
-            return bindingResult.getFieldErrors().stream()
-                    .map(error -> new FieldError(
-                            error.getField(),
-                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
-                            error.getDefaultMessage()
-                    )).collect(Collectors.toList());
-        }
+        return new ResponseEntity<>(this, this.error.getHttpStatus());
     }
 }
---
+++ src/main/java/com/youngxpepp/instagramcloneserver/global/error/type/BindingErrorType.java
@@ -0,0 +1,57 @@
+package com.youngxpepp.instagramcloneserver.global.error.type;
+
+import com.youngxpepp.instagramcloneserver.global.error.ErrorCode;
+import lombok.Getter;
+import org.springframework.validation.BindingResult;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class BindingErrorType extends DefaultErrorType {
+
+    public BindingErrorType(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public BindingErrorType(ErrorCode errorCode, BindingResult bindingResult) {
+        this(errorCode);
+        super.details.addAll(BindingDetails.of(bindingResult));
+    }
+
+    public BindingErrorType(ErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
+
+    public BindingErrorType(ErrorCode errorCode, String message, BindingResult bindingResult) {
+        this(errorCode, message);
+        super.details.addAll(BindingDetails.of(bindingResult));
+    }
+
+    @Getter
+    public static class BindingDetails {
+        private String field;
+        private String value;
+        private String reason;
+
+        private BindingDetails(String field, String value, String reason) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+
+        public static List<BindingDetails> of(String field, String value, String reason) {
+            return Arrays.asList(new BindingDetails(field, value, reason));
+        }
+
+        public static List<BindingDetails> of(BindingResult bindingResult) {
+            return bindingResult.getFieldErrors().stream()
+                    .map(error -> new BindingDetails(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()
+                    )).collect(Collectors.toList());
+        }
+    }
+}
