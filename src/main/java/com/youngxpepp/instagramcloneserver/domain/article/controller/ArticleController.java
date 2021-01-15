bbc0990c30cc857abc93cd06459298cb71130856
youngxpepp <youngxpepp@gmail.com>
2021-01-15 17:43:47 +0900

    fix: 게시물 작성 API 컨트롤러 수정
    - 컨트롤러에 RequestMapping 추가

--- src/main/java/com/youngxpepp/instagramcloneserver/domain/article/controller/ArticleController.java
+++ src/main/java/com/youngxpepp/instagramcloneserver/domain/article/controller/ArticleController.java
@@ -6,14 +6,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import com.youngxpepp.instagramcloneserver.domain.article.dto.CreateArticleRequestBody;
 import com.youngxpepp.instagramcloneserver.domain.article.dto.CreateArticleResponseBody;
 import com.youngxpepp.instagramcloneserver.domain.article.service.ArticleService;
 import com.youngxpepp.instagramcloneserver.domain.member.model.Member;
 
-@RestController("/api/v1/articles")
+@RestController
+@RequestMapping("/api/v1/articles")
 @RequiredArgsConstructor
 public class ArticleController {
 
@@ -21,7 +24,7 @@ public class ArticleController {
 
 	@PostMapping
 	public ResponseEntity<CreateArticleResponseBody> createArticle(
-		@AuthenticationPrincipal Member principal,
+		@AuthenticationPrincipal @ApiIgnore Member principal,
 		@RequestBody CreateArticleRequestBody requestBody) {
 		CreateArticleResponseBody responseBody = articleService.createArticle(principal.getId(), requestBody);
 		return new ResponseEntity<>(responseBody, HttpStatus.CREATED);
