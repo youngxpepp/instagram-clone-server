57aa00e80935c121421dfc697af6efc89107d1b5
youngxpepp <youngxpepp@gmail.com>
2021-01-30 17:24:16 +0900

    feat: 댓글 생성 API 시간 반환 형식 변경
    - 기존에는 배열 형식으로 반환했음
    - yyyy-MM-ddTHH:mm:ss 으로 반환될 수 있도록 변경

--- src/main/java/com/youngxpepp/instagramcloneserver/domain/article/dto/CreateCommentResponseBody.java
+++ src/main/java/com/youngxpepp/instagramcloneserver/domain/article/dto/CreateCommentResponseBody.java
@@ -2,6 +2,7 @@ package com.youngxpepp.instagramcloneserver.domain.article.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +14,10 @@ public class CreateCommentResponseBody {
 
 	private final Long id;
 	private final String content;
+
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", shape = JsonFormat.Shape.STRING, timezone = "Asia/Seoul")
 	private final LocalDateTime createdAt;
+
 	private final CreatedBy createdBy;
 
 	@RequiredArgsConstructor
